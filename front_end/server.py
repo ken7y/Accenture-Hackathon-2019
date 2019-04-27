10f21b3cfe2ce8a03ba1d446e69128babb016600
Joanna Chan <joanna.wl.chan@gmail.com>
2019-04-28 00:23:40 +1000

    front-end updated

--- front_end/server.py
+++ front_end/server.py
@@ -8,5 +8,17 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/australia')
+def australia():
+    return render_template('australia.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
