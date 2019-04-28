04337844b03ca94bafff3b1bd4e38c34df9352d7
Samuel <37462063+samuelzhang97@users.noreply.github.com>
2019-04-28 11:00:22 +1000

    update

--- blockchain/contracts/BuyGoods.sol
+++ blockchain/contracts/BuyGoods.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.5.0;
 
-// code based on https://github.com/jklepatch/eattheblocks/blob/master/special-episode-1/step3/contracts/ToDo.sol 
-
 contract buyGoods {
 	string eventName;
 	uint public total;
@@ -12,21 +10,19 @@ contract buyGoods {
 	uint public threshold;
 	Donator[] public donators; // the buyers
 	address payable public supplier; // the seller
-	address public admin; // the administration - we can cancel the funds
 	bool paid;
 	enum State { Created, Locked, Inactive }
 	State public state;
 
 	// Create the contract 
-	constructor(string memory eN, address payable s, uint t) 
+	constructor(string memory _eventName, address payable _supplier, uint _threshold) 
 		payable
 		public  
 	{
-		supplier = s;
-		eventName = eN;
-		admin = msg.sender;
-		paid = false;
-		threshold = t;
+		supplier = _supplier;
+		eventName = _eventName;
+		threshold = _threshold;
+		total = 0;
 	}
 
 	modifier condition(bool _condition) {
@@ -55,21 +51,6 @@ contract buyGoods {
 	event ThresholdReached();
 	//event PurchaseConfirmed();
 
-	// Cancel the donations if threshold not met, can only do as admin
-	function cancel()
-		public
-		onlyAdmin()
-		inState(State.Created) 
-	{
-		emit Cancelled();
-		state = State.Inactive;
-		uint len = donators.length;
-		// refund all donators how much they donated if the threshold is not met
-		for (uint i = 0; i < len; i++) {
-			donators[i].dAddr.transfer(donators[i].value);
-		}
-	}
-
 	function addDonation()
 		public
 		payable
@@ -79,14 +60,11 @@ contract buyGoods {
 			dAddr: msg.sender,
 			value: msg.value
 		}));
-		total += msg.value;
+		total = total + msg.value;
 		if (total >= threshold) {
 			emit ThresholdReached();
 		}
 	}
 
-	function getTotal() public returns (uint){
-		return total;
-	}
 
 }
